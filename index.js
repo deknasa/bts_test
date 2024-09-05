38d572510bf019a5269ed565189d4e8b1c3b5bb9
deknasa <80618997+deknasa@users.noreply.github.com>
2024-09-05 18:20:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 8080
 const userRoutes = require('./routes/users')
 
 app.use(express.json())
 
-app.use('/user', userRoutes)
+app.use('/api', userRoutes)
 
 if (process.env.PORT != "test") {
     app.listen(port, () => console.log(`server running on port ${port}`))
